18d77ae44ed07688f6ad755b287060c892369e58
Leo Antunes <leo.antunes@regiohelden.de>
2018-10-02 15:08:10 +0200

    aliases changes

--- aliases.sh
+++ aliases.sh
@@ -2,10 +2,14 @@ alias dc=docker-compose
 alias dce='docker-compose exec'
 alias dcr='docker-compose run --rm --label traefik.enable=False'
 alias dcb='docker-compose build --pull'
-alias dcb='docker-compose build --pull'
 
-alias dcds='docker-compose run --rm --label traefik.enable=False django shell_plus'
-alias dcm='docker-compose run --rm --label traefik.enable=False django migrate'
-alias dcmm='docker-compose run --rm --label traefik.enable=False django makemigrations'
-alias dci='docker-compose run --rm --label traefik.enable=False django i18n'
-alias dcrs='docker-compose run --rm django runserver 0:8000'
+alias djs='dcr django shell_plus'
+alias djm='dcr django migrate'
+alias djmm='dcr django makemigrations'
+alias dji='dcr django i18n'
+alias djt='dcr -e DJANGO_CONFIGURATION=Test django test --no-input --parallel=$(python -c "import multiprocessing; print(multiprocessing.cpu_count())")'
+alias djrc='dcr --entrypoint=celery django worker -A $(basename $(pwd))'
+# unlike the aliases above, this will make the running process accessible via the proxy, so this
+# can be used for interactive debugging if you stop the "django" service with `dc stop django`
+# beforehand
+alias djrs='docker-compose run --rm django runserver 0:8000'
