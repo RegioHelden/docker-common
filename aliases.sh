976bc6918e8d1ab45724437390f4fdbbf2b73f74
Leo Antunes <leo.antunes@regiohelden.de>
2018-10-05 12:04:49 +0200

    aliases: also build after djpip

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,7 @@
 alias dc=docker-compose
 alias dce='docker-compose exec'
 alias dcr='docker-compose run --rm --label traefik.enable=False'
-alias dcb='docker-compose build --pull'
+alias dcb='docker-compose build'
 
 alias djs='dcr django shell_plus'
 alias djm='dcr django migrate'
@@ -9,7 +9,7 @@ alias djmm='dcr django makemigrations'
 alias dji='dcr django i18n'
 alias djt='dcr -e DJANGO_CONFIGURATION=Test django test --no-input --parallel=$(python -c "import multiprocessing; print(multiprocessing.cpu_count())")'
 alias djrc='dcr --entrypoint=celery django worker -A $(basename $(pwd))'
-alias djpip='dcr --entrypoint "bash -c \"which pipenv && pipenv lock || pip-compile\"" django'
+alias djpip='dcr --entrypoint "bash -c \"which pipenv && pipenv lock || pip-compile\"" django && dcb'
 # unlike the aliases above, this will make the running process accessible via the proxy, so this
 # can be used for interactive debugging if you stop the "django" service with `dc stop django`
 # beforehand
