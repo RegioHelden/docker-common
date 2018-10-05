13ba520bb0fa8d18079196be2e393d185022d5b3
Leo Antunes <leo.antunes@regiohelden.de>
2018-10-05 11:55:37 +0200

    add djpip alias

--- aliases.sh
+++ aliases.sh
@@ -9,6 +9,7 @@ alias djmm='dcr django makemigrations'
 alias dji='dcr django i18n'
 alias djt='dcr -e DJANGO_CONFIGURATION=Test django test --no-input --parallel=$(python -c "import multiprocessing; print(multiprocessing.cpu_count())")'
 alias djrc='dcr --entrypoint=celery django worker -A $(basename $(pwd))'
+alias djpip='dcr --entrypoint "bash -c \"which pipenv && pipenv lock || pip-compile\"" django'
 # unlike the aliases above, this will make the running process accessible via the proxy, so this
 # can be used for interactive debugging if you stop the "django" service with `dc stop django`
 # beforehand
