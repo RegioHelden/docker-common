f4cb55500d1363b1656c3959c4c6cafac48e4c59
Leo Antunes <leo@costela.net>
2018-10-18 12:55:13 +0200

    fix aliases for new angular images with entrypoint=npm

--- aliases.sh
+++ aliases.sh
@@ -25,6 +25,6 @@ alias dcdb='dce db psql app'
 
 # angular aliases
 alias ng='dcr angular run-script ng'
-alias ngg='dcr angular npm run-script ng g -- --spec=false'
-alias ngl='dcr angular npm run-script ng lint --'
-alias ngi='dcr angular npm run-script i18n'
+alias ngg='dcr angular run-script ng g -- --spec=false'
+alias ngl='dcr angular run-script ng lint --'
+alias ngi='dcr angular run-script i18n'
