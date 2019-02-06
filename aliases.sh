3f345cc16b677763ab46bc164d9c08c20a124305
Leo Antunes <leo@costela.net>
2019-02-06 14:53:18 +0100

    aliases: stop django before running runserver interactively

--- aliases.sh
+++ aliases.sh
@@ -13,11 +13,11 @@ alias dji='dcr django i18n'
 alias djt='dcr -e DJANGO_CONFIGURATION=Test django test --no-input --parallel=$(python -c "import multiprocessing; print(multiprocessing.cpu_count())")'
 alias djrc='dcr --entrypoint=celery django worker -A $(basename $(pwd))'
 alias djpip='dcr --entrypoint "bash -c \"which pipenv && pipenv lock || pip-compile\"" django && dcb'
+alias djdocs='docker-compose run --rm --entrypoint "bash -c" django "cd docs && make html -s"'
 # unlike the aliases above, this will make the running process accessible via the proxy, so this
 # can be used for interactive debugging if you stop the "django" service with `dc stop django`
 # beforehand
-alias djrs='docker-compose run --rm --use-aliases django runserver 0:8000'
-alias djdocs='docker-compose run --rm --entrypoint "bash -c" django "cd docs && make html -s"'
+alias djrs='dc stop django; docker-compose run --rm --use-aliases django runserver 0:8000'
 
 
 # postgres aliases
